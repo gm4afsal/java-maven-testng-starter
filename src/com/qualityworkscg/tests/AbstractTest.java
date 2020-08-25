74b0cbc1c793b4d2798f15a68aee14d1fe783138
gm4afsal <63408724+gm4afsal@users.noreply.github.com>
2020-08-25 23:16:51 +0530

    Update AbstractTest.java

--- src/com/qualityworkscg/tests/AbstractTest.java
+++ src/com/qualityworkscg/tests/AbstractTest.java
@@ -1,6 +1,10 @@
 package com.qualityworkscg.tests;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
@@ -16,14 +20,33 @@ public abstract class  AbstractTest {
   @BeforeTest
   @Parameters({"url"})
   public void setup(String url) {
-    // Set the path to the geckodriver
+
 	  System.out.println("Browser: Chrome");
       
       WebDriverManager.chromedriver().setup();
     
-    // Instantiate a new Page and navigate 
-    // to the url specified in the testng.xml
-    page = new Page(new ChromeDriver());
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      
+      ChromeOptions options = new ChromeOptions();
+      
+      
+      options.merge(capabilities);
+
+      options.addArguments("--headless");
+      
+      options.addArguments("start-maximized"); 
+      options.addArguments("enable-automation"); 
+      options.addArguments("--no-sandbox"); 
+      options.addArguments("--disable-infobars"); 
+      options.addArguments("--disable-dev-shm-usage"); //Linux 
+      options.addArguments("--disable-browser-side-navigation"); 
+      options.addArguments("--disable-gpu"); //Windows
+      
+      options.addArguments("--disable-web-security");
+      
+       
+      
+    page = new Page(new ChromeDriver(options));
     page.navigate(url);
   }
 
